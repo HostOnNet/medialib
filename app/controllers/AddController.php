afaee56362beb8a43649588743ff31e271a4977d
WebHostingNeeds.com <info@webhostingneeds.com>
2014-02-23 05:15:19 +0530

    fixed thumbnail creation. replace tabs with space

--- app/controllers/AddController.php
+++ app/controllers/AddController.php
@@ -2,55 +2,55 @@
 
 class AddController extends BaseController
 {
-	public function index()
-	{
-		$this->layout->title = "Add";
-		$this->layout->nest('content','add.index');
-	}
+    public function index()
+    {
+        $this->layout->title = "Add";
+        $this->layout->nest('content','add.index');
+    }
 
-	public function save()
-	{
-		if (!isset($_POST['submit']))
-		{
-			die('NO submit');
-		}
+    public function save()
+    {
+        if (!isset($_POST['submit']))
+        {
+            die('NO submit');
+        }
 
-		$file_name = trim($_POST['file_name']);
+        $file_name = trim($_POST['file_name']);
 
-		$file_path = Config::get('app.video_folder') . $file_name;
+        $file_path = Config::get('app.video_folder') . $file_name;
 
-		if (!file_exists($file_path)) die("File not found $file_path");
+        if (!file_exists($file_path)) die("File not found $file_path");
 
         if (!is_file($file_path))
         {
             die('Not a valid file: ' . $file_path);
         }
 
-		$msg = '';
+        $msg = '';
 
-		$time_start =  '00:00:00';
-		$time_end = $this->find_media_duration($file_name);
+        $time_start =  '00:00:00';
+        $time_end = $this->find_media_duration($file_name);
 
-    	$description = "00:00:00=todo";
+        $description = "00:00:00=todo";
 
-		$num_records = Media::where('file_name', '=', $file_name)->where('time_start_hms', '=', $time_start)->count();
+        $num_records = Media::where('file_name', '=', $file_name)->where('time_start_hms', '=', $time_start)->count();
 
-		if ($num_records == 0)
-		{
-			$media_id = Media::insertGetId(array('file_name' => "$file_name", 'time_start_hms' => "$time_start", 'time_end_hms' => "$time_end", 'description' => "$description" ));
-			Tags::add($description, $media_id);
-			$msg .=  "<p>Added - $file_name ($time_start - $time_end)</p>";
-		}
-		else
-		{
-			$msg .= "<p>SKIP - $file_name ($time_start - $time_end)</p>";
-		}
+        if ($num_records == 0)
+        {
+            $media_id = Media::insertGetId(array('file_name' => "$file_name", 'time_start_hms' => "$time_start", 'time_end_hms' => "$time_end", 'description' => "$description" ));
+            Tags::add($description, $media_id);
+            $msg .=  "<p>Added - $file_name ($time_start - $time_end)</p>";
+        }
+        else
+        {
+            $msg .= "<p>SKIP - $file_name ($time_start - $time_end)</p>";
+        }
 
-		Backup::db(1);
+        Backup::db(1);
 
-		$this->layout->title = "DONE";
-		$this->layout->nest('content','add.done', array('msg' => $msg) );
-	}
+        $this->layout->title = "DONE";
+        $this->layout->nest('content','add.done', array('msg' => $msg) );
+    }
 
     function find_media_duration($file_name)
     {
@@ -74,226 +74,146 @@ class AddController extends BaseController
         return $duration;
     }
 
+    function create_thumb($srcname, $new_image_file, $new_width, $new_height)
+    {
+        $imagedata = GetImageSize($srcname);
+        $imagewidth = $imagedata[0];
+        $imageheight = $imagedata[1];
 
-/*	function validate_media($raw_data_lines)
-	{
+        $src_img = imagecreatefromjpeg($srcname);
+        $new_image = imageCreateTrueColor($new_width, $new_height);
+        ImageCopyResampled($new_image, $src_img, 0, 0, 0, 0, $new_width, $new_height, $imagewidth, $imageheight);
+        imagejpeg($new_image, $new_image_file, 100);
+        imagedestroy($src_img);
+        imagedestroy($new_image);
 
-		$parts = explode('|',$raw_data_lines[0]);
+    }
 
-		if (isset($parts[2]))
-		{
-			$regex = '/^([0-9]{2}\:[0-9]{2}\:[0-9]{2})\s+\|\s+([0-9]{2}\:[0-9]{2}\:[0-9]{2})\s+\|\s+.*$/i';
-		}
-		else
-		{
-			$regex = '/^([0-9]{2}\:[0-9]{2}\:[0-9]{2})\s+\|\s+([0-9]{2}\:[0-9]{2}\:[0-9]{2})\s*$/i';
-		}
-
-		foreach ($raw_data_lines as $raw_data_line)
-		{
-			$raw_data_line = trim($raw_data_line);
-
-			if (!preg_match($regex, $raw_data_line, $matches))
-			{
-				die("ERROR on Line:<br>$raw_data_line");
-			}
-			else
-			{
-				$time_start = Time::hms2sec($matches[1]);
-				$time_end = Time::hms2sec($matches[2]);
-				$duration = $time_end - $time_start;
-
-				if ($duration < 30)
-				{
-					die("Video duration is too small: <b>$duration</b> seconds");
-				}
-			}
-		}
-	}*/
+    function thumb()
+    {
+        $media_list = Media::get();
 
 
-	function create_thumb($srcname, $destname, $maxwidth, $maxheight)
-	{
-		$oldimg = $srcname;
-		$newimg = $destname;
+        $msg = '';
 
-		$imagedata = GetImageSize($oldimg);
-		$imagewidth = $imagedata[0];
-		$imageheight = $imagedata[1];
-		$imagetype = $imagedata[2];
+        $video_folder = Config::get('app.video_folder');
+        $thumb_folder = Config::get('app.thumb_folder');
+        $base_folder = public_path();
+        $num_thumb_created = 0;
 
-		$shrinkage = 1;
+        foreach($media_list as $media)
+        {
+            $media_id = $media->id;
+            $media_file_name = $media->file_name;
+            $time_start_sec = Time::hms2sec($media->time_start_hms);
+            $time_end_sec = Time::hms2sec($media->time_end_hms);
+            $time = rand($time_start_sec, $time_end_sec);
 
-		if ($imagewidth > $maxwidth)
-		{
-			$shrinkage = $maxwidth / $imagewidth;
-		}
-		if ($shrinkage != 1)
-		{
-			$dest_height = $shrinkage * $imageheight;
-			$dest_width = $maxwidth;
-		}
-		else
-		{
-			$dest_height = $imageheight;
-			$dest_width = $imagewidth;
-		}
+            if ($time < 1)
+            {
+                $msg .= 'Error processing video with id ' . $media_id . "\n";
+                continue;
+            }
 
-		if ($dest_height > $maxheight)
-		{
-			$shrinkage = $maxheight / $dest_height;
-			$dest_width = $shrinkage * $dest_width;
-			$dest_height = $maxheight;
-		}
+            $file_path = $video_folder  . $media_file_name;
 
-		if ($imagetype == 2)
-		{
-			$src_img = imagecreatefromjpeg($oldimg);
-			$dst_img = imageCreateTrueColor($dest_width, $dest_height);
-			ImageCopyResampled($dst_img, $src_img, 0, 0, 0, 0, $dest_width, $dest_height, $imagewidth, $imageheight);
-			imagejpeg($dst_img, $newimg, 100);
-			imagedestroy($src_img);
-			imagedestroy($dst_img);
-		}
-		elseif ($imagetype == 3)
-		{
-			$src_img = imagecreatefrompng($oldimg);
-			$dst_img = imageCreateTrueColor($dest_width, $dest_height);
-			ImageCopyResampled($dst_img, $src_img, 0, 0, 0, 0, $dest_width, $dest_height, $imagewidth, $imageheight);
-			imagepng($dst_img, $newimg, 100);
-			imagedestroy($src_img);
-			imagedestroy($dst_img);
-		}
-		else
-		{
-			$src_img = imagecreatefromgif($oldimg);
-			$dst_img = imageCreateTrueColor($dest_width, $dest_height);
-			ImageCopyResampled($dst_img, $src_img, 0, 0, 0, 0, $dest_width, $dest_height, $imagewidth, $imageheight);
-			imagejpeg($dst_img, $newimg, 100);
-			imagedestroy($src_img);
-			imagedestroy($dst_img);
-		}
-	}
+            if (!file_exists($file_path))
+            {
+                die('Video Not Found ' . $file_path);
+            }
 
-	function thumb()
-	{
-		$media_list = Media::get();
+            $thumb_path = $base_folder . '\\' . $thumb_folder . $media_id . '.jpg';
+            $thumb_path_tmp = $base_folder . '\\' . $thumb_folder . 'tmp_' . $media_id. '.jpg';
+            $thumb_url =  '/' . $thumb_folder . '\\' . $media_id . '.jpg';
 
+            if (file_exists($thumb_path))
+            {
+                 continue;
+            }
 
-		$msg = '';
+            $cmd = "G:\\Programs\\ffmpeg\\bin\\ffmpeg.exe  -ss $time  -i \"$file_path\" $thumb_path_tmp -r 1 -vframes 1 -an -vcodec mjpeg";
+            $msg .= "$cmd\n";
+            flush();
+            exec($cmd);
+            $num_thumb_created++;
 
-		$video_folder = Config::get('app.video_folder');
-		$thumb_folder = Config::get('app.thumb_folder');
-		$base_folder = public_path();
+            if (file_exists("$thumb_path_tmp"))
+            {
+                $this->create_thumb("$thumb_path_tmp", "$thumb_path", 384, 216);
+                unlink("$thumb_path_tmp");
+                $msg .= "<a href=/watch/$media_id/media-1><img src=$thumb_url></a> \n \n";
+            }
+            else
+            {
+                $msg .= "Failed to create thumbnail for $media_id\n";
+            }
 
-		foreach($media_list as $media)
-		{
-			$media_id = $media->id;
-			$media_file_name = $media->file_name;
-			$time_start_sec = Time::hms2sec($media->time_start_hms);
-			$time_end_sec = Time::hms2sec($media->time_end_hms);
-			$time = rand($time_start_sec, $time_end_sec);
+            if ($num_thumb_created > 12)
+            {
+                return "<script language=\"JavaScript\"><!-- setTimeout('window.location.href = \"\"',8000); --></script>wait....";
+                exit;
+            }
+        }
 
-			if ($time < 1)
-			{
-				$msg .= 'Error processing video with id ' . $media_id . "\n";
-				continue;
-			}
+        $this->layout->title = "Thumb Make";
+        $this->layout->nest('content','add.thumb', array('msg' => $msg) );
+    }
 
-			$file_path = $video_folder  . $media_file_name;
+    public function thumb_single($media_id, $time)
+    {
+        $msg = '';
 
-			if (!file_exists($file_path))
-			{
-				die('Video Not Found ' . $file_path);
-			}
+        $media = Media::find($media_id);
+        $media_file_name = $media->file_name;
+        $time = round ($time/1000);
 
-			$thumb_path = $base_folder . '\\' . $thumb_folder . $media_id . '.jpg';
-			$thumb_path_tmp = $base_folder . '\\' . $thumb_folder . 'tmp_' . $media_id. '.jpg';
-			$thumb_url =  '/' . $thumb_folder . '\\' . $media_id . '.jpg';
+        $video_folder = Config::get('app.video_folder');
+        $thumb_folder = Config::get('app.thumb_folder');
+        $base_folder = public_path();
 
-			if (file_exists($thumb_path))
-			{
-				 continue;
-			}
+        $file_path = $video_folder  . $media_file_name;
 
-			$cmd = "G:\\Programs\\ffmpeg\\bin\\ffmpeg.exe  -ss $time  -i \"$file_path\" $thumb_path_tmp -r 1 -vframes 1 -an -vcodec mjpeg";
-			$msg .= "$cmd\n";
-			flush();
-			exec($cmd);
-
-			if (file_exists("$thumb_path_tmp"))
-			{
-				$this->create_thumb("$thumb_path_tmp", "$thumb_path", 384, 216);
-				unlink("$thumb_path_tmp");
-				$msg .= "<a href=/watch/$media_id/media-1><img src=$thumb_url></a> \n \n";
-			}
-			else
-			{
-				$msg .= "Failed to create thumbnail for $media_id\n";
-			}
-
-
-		}
-
-		$this->layout->title = "Thumb Make";
-		$this->layout->nest('content','add.thumb', array('msg' => $msg) );
-	}
-
-	public function thumb_single($media_id, $time)
-	{
-		$msg = '';
-
-		$media = Media::find($media_id);
-		$media_file_name = $media->file_name;
-		$time = round ($time/1000);
-
-		$video_folder = Config::get('app.video_folder');
-		$thumb_folder = Config::get('app.thumb_folder');
-		$base_folder = public_path();
-
-		$file_path = $video_folder  . $media_file_name;
-
-		if (!file_exists($file_path))
-		{
-			die('Video Not Found');
-		}
+        if (!file_exists($file_path))
+        {
+            die('Video Not Found');
+        }
 
         if (!is_dir($base_folder . '\\' . $thumb_folder))
         {
             die('Thumb folder not found ');
         }
 
-		$thumb_path = $base_folder . '\\' . $thumb_folder . $media_id . '.jpg';
-		$thumb_path_tmp = $base_folder . '\\' . $thumb_folder . 'tmp_' . $media_id. '.jpg';
-		$thumb_url =  '/' .  $thumb_folder . '\\' . $media_id . '.jpg';
+        $thumb_path = $base_folder . '\\' . $thumb_folder . $media_id . '.jpg';
+        $thumb_path_tmp = $base_folder . '\\' . $thumb_folder . 'tmp_' . $media_id. '.jpg';
+        $thumb_url =  '/' .  $thumb_folder . '\\' . $media_id . '.jpg';
 
-		$cmd = "G:\\Programs\\ffmpeg\\bin\\ffmpeg.exe  -ss $time  -i \"$file_path\" $thumb_path_tmp -r 1 -vframes 1 -an -vcodec mjpeg";
-		$msg .= "$cmd\n";
-		flush();
-		exec($cmd);
+        $cmd = "G:\\Programs\\ffmpeg\\bin\\ffmpeg.exe  -ss $time  -i \"$file_path\" $thumb_path_tmp -r 1 -vframes 1 -an -vcodec mjpeg";
+        $msg .= "$cmd\n";
+        flush();
+        exec($cmd);
 
-		if (file_exists("$thumb_path_tmp"))
-		{
-			if (file_exists ($thumb_path))
-			{
-				if (!unlink($thumb_path))
-				{
-					die("Failed to delete $thumb_path");
-				}
-			}
+        if (file_exists("$thumb_path_tmp"))
+        {
+            if (file_exists ($thumb_path))
+            {
+                if (!unlink($thumb_path))
+                {
+                    die("Failed to delete $thumb_path");
+                }
+            }
 
-			$this->create_thumb("$thumb_path_tmp", "$thumb_path", 384, 216);
-			unlink("$thumb_path_tmp");
-			$msg .= "<img src=$thumb_url?time=" . time() . ">";
-		}
-		else
-		{
-			$msg .= "Failed to create thumbnail for $media_id\n";
-		}
+            $this->create_thumb("$thumb_path_tmp", "$thumb_path", 384, 216);
+            unlink("$thumb_path_tmp");
+            $msg .= "<img src=$thumb_url?time=" . time() . ">";
+        }
+        else
+        {
+            $msg .= "Failed to create thumbnail for $media_id\n";
+        }
 
-		$this->layout->title = "Thumb Make";
-		$this->layout->nest('content','add.thumb', array('msg' => $msg) );
-	}
+        $this->layout->title = "Thumb Make";
+        $this->layout->nest('content','add.thumb', array('msg' => $msg) );
+    }
 
     public function rebuild_media_likes()
     {
