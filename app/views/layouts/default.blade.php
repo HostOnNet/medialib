85ce9a517830af911df57ad509f2563aae11c52a
WebHostingNeeds.com <info@webhostingneeds.com>
2014-02-24 06:15:17 +0530

    jquery updated.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script language="JavaScript" type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+<script language="JavaScript" type="text/javascript" src="/js/jquery-1.11.0.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="/css/style.css" />
