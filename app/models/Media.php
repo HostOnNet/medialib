4b88ea08884723441bbecd56bcbbeb21d3fb46ac
WebHostingNeeds.com <info@webhostingneeds.com>
2014-08-15 09:35:40 +0530

    updated viewAfterDays

--- app/models/Media.php
+++ app/models/Media.php
@@ -7,24 +7,24 @@ class Media extends Eloquent
     public static function getViewAgain($views, $likes)
     {
         if ($views < 10) {
-            $viewAfterDays = 2;
+            $viewAfterDays = 10;
         } else if ($views < 20) {
-            $viewAfterDays = 5;
+            $viewAfterDays = 20;
         } else {
             if ($likes == 0) $likes = 1;
             $likesPerView = ($likes/$views) * 100;
             if ($likesPerView > 90) {
-                $viewAfterDays = 2;
+                $viewAfterDays = 6;
             } elseif ($likesPerView > 80) {
-                $viewAfterDays = 5;
+                $viewAfterDays = 15;
             } elseif ($likesPerView > 70) {
-                $viewAfterDays = 7;
-            } elseif ($likesPerView > 60) {
-                $viewAfterDays = 12;
-            } elseif ($likesPerView > 40) {
                 $viewAfterDays = 20;
+            } elseif ($likesPerView > 60) {
+                $viewAfterDays = 30;
+            } elseif ($likesPerView > 40) {
+                $viewAfterDays = 50;
             } else {
-                $viewAfterDays = 40;
+                $viewAfterDays = 100;
             }
         }
         return time() + ($viewAfterDays * 86400);
