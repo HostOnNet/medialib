f5ba789f483c50365cf975caec897b05f458380d
WebHostingNeeds.com <info@webhostingneeds.com>
2014-09-20 01:49:51 +0530

    highlight auto play time.

--- app/views/watch/show.blade.php
+++ app/views/watch/show.blade.php
@@ -13,12 +13,11 @@
 
 
 {{ Form::open(['method' => 'post', 'id' => 'media_edit', 'class' => 'form-horizontal', 'url' =>  '/media/save' ]) }}
-
+{{ Form::hidden('media_id', $media->id) }}
+{{ Form::hidden('ref_page', $ref_page) }}
 
 <div id="watch_col_form">
-        <input type="hidden" name="media_id" value="{{ $media->id }}">
-        <input type="hidden" name="ref_page" value="{{ $ref_page }}">
-        <textarea name="description" id="txt_description" rows="5" cols="70" disabled>{{ Tags::sort_bookmark($media->description) }}</textarea>
+    <textarea name="description" id="txt_description" rows="5" cols="70" disabled>{{ Tags::sort_bookmark($media->description) }}</textarea>
 </div>
 
 <div id="watch_col_player">
@@ -26,7 +25,7 @@
            autoplay="no"
            loop="no"
            toolbar="no"
-           volume="100"
+           volume="{{ $media->volume }}"
            width="720"
            height="406"
            target="file:///{{ Config::get('app.video_folder') }}/tmp/live.m3u">
@@ -42,7 +41,7 @@
 
 	<div id="watch_time">
         <div id="currentTime">00:00:00.000</div> &nbsp; &nbsp;
-        <div id="wt_2"><?php echo "$media->time_start_hms - $media->time_end_hms"; ?>  &nbsp; <?php echo $last_viewed; ?></div>
+        <div id="wt_2"><?php echo "$media->time_start_hms - $media->time_end_hms"; ?>  &nbsp; {{ $last_viewed }}</div>
         <div id="skip_time_remaining"></div>
         <div id="skip_time_more"></div>
     </div>
@@ -51,7 +50,7 @@
 
     <div id="watch_controls">
         <div class="form-inline">
-            <input type="number" id="volume_input" name="volume" value="<?php echo $media->volume; ?>" min=20 max=200 class="form-control" style="width:90px">
+            <input type="number" id="volume_input" name="volume" value="{{ $media->volume }}" min=20 max=200 class="form-control" style="width:90px">
             <input type="submit" name="submit" value="Next >>" class="btn btn-default">
             {{ $videos_in_playlist }}
         </div>
@@ -85,7 +84,7 @@ if ($auto_play_time_hms === false) {
     $time_end_ms = 0;
 } else {
     $time_start_ms = Time::hmsms2ms($auto_play_time_hms);
-    $time_end_ms = $time_start_ms + 10000;
+    $time_end_ms = $time_start_ms + (Settings::get('autoForwardDuration') * 1000);
 }
 
 ?>
@@ -113,6 +112,23 @@ if ($auto_play_time_hms === false) {
             end_time = end_time + 30000;
         });
 
+        // highlight current auto play time.
+
+        var description = "{{ $media->description }}";
+        var current_playlist_tag_time = "{{ $auto_play_time_hms  }}";
+        var bookmarks = description.split("|");
+        var bookmark = '';
+        var num_bookmakrs = bookmarks.length;
+
+        for (var i = 0; i < num_bookmakrs; i++) {
+            bookmark = bookmarks[i];
+            var bookmark_parts = bookmark.split("=");
+            var bookmark_time = $.trim(bookmark_parts[0]);
+            if (bookmark_time == current_playlist_tag_time) {
+                $("a[alt='" + bookmark_time + "']").addClass("current_playlist_tag");
+            }
+        }
+
     });
 </script>
 @endif
@@ -120,45 +136,3 @@ if ($auto_play_time_hms === false) {
 @if ($time_end_ms > 0)
 <script>end_time = {{ $time_end_ms }};</script>
 @endif
-
-
-<?php
-
-if (isset($skip_to_bookmark_done))
-{
-
-?>
-
-<script type="text/javascript">
-jQuery(function () {
-
-    var description = "<?php echo $media->description; ?>";
-    var current_playlist_tag_time = "<?php echo $time_start; ?>";
-    var bookmarks = description.split("|");
-    var bookmark = '';
-    var num_bookmakrs = bookmarks.length;
-
-    for (var i = 0; i < num_bookmakrs; i++)
-    {
-        bookmark = bookmarks[i];
-        var bookmark_parts = bookmark.split("=");
-        var bookmark_time = $.trim(bookmark_parts[0]);
-
-        if (bookmark.indexOf("<?php echo $skip_to_bookmark; ?>") != -1)
-        {
-            if (bookmark_time == current_playlist_tag_time)
-            {
-                $("a[alt='" + bookmark_time + "']").addClass("current_playlist_tag current_playlist_tag_more");
-            }
-            else
-            {
-                $("a[alt='" + bookmark_time + "']").addClass("current_playlist_tag_more");
-            }
-        }
-    }
-
-});
-</script>
-<?php
-}
-?>
