3c13d7e31809a7ab40a721fc68f58c6f6f955a87
WebHostingNeeds.com <info@webhostingneeds.com>
2014-11-28 12:58:45 +0530

    playlist file css changed

--- app/views/playlist/list.blade.php
+++ app/views/playlist/list.blade.php
@@ -1,32 +1,44 @@
-<h1>Playlists</h1>
+<div class="page-header">
+    <h1>Playlists</h1>
+</div>
 
 
-
-<?php
-
-foreach($playlists as $playlist)
-{
-    echo "<div class=\"btn-group\">";
-    echo "<a href=/playlist/" . $playlist->id . " class=\"btn btn-success\">" . $playlist->name  . " <span class=\"badge\">" . $playlist->total . "</span></a>";
-    echo "<a href=/playlist/empty/" . $playlist->id . " class=\"btn btn-danger\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></a>";
-    echo "</div>";
-    echo "<br> <br>";
-}
-
-?>
-
-<h1>Build Playlists</h1>
-
-<a href=/playlist_seeds  class="btn btn-success">Playlist Seeds</a>
-<a href=/playlist_make/2 class="btn btn-success">Best Medias</a>
-<a href=/playlist_make/1 class="btn btn-success">Best Tags</a>
+<a href=/playlist_seeds  class="btn btn-lg btn-success">Playlist Seeds</a>
+<a href=/playlist_make/2 class="btn btn-lg btn-success">Best Medias</a>
+<a href=/playlist_make/1 class="btn btn-lg btn-success">Best Tags</a>
 
 <?php
 
 $tag_id = Tags::getId('todo');
 if ($tag_id) {
-    echo '<a href=/tag/' . $tag_id . ' class="btn btn-success">Todo</a>';
+    echo '<a href=/tag/' . $tag_id . ' class="btn btn-lg btn-success">Todo</a>';
 }
 
 ?>
+
+<br>
+
+<br>
+
+
+<h2>Recent</h2>
+
+<div class="row">
+    <div class="col-md-12">
+
+        @foreach ($playlists as $playlist)
+
+        <div class="btn-group">
+            <a href="/playlist/{{ $playlist->id }}" class="btn btn-info">
+                {{ $playlist->name }} <span class="badge">{{ $playlist->total }}</span>
+            </a>
+            <a href="/playlist/empty/{{ $playlist->id }}" class="btn btn-danger">
+                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+            </a>
+        </div> &nbsp;  &nbsp;
+
+        @endforeach
+
+    </div>
+</div>
 
