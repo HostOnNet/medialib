7eab1d8b0eaa529752130c35a4cda0cc043892cf
WebHostingNeeds.com <info@webhostingneeds.com>
2014-11-28 07:37:39 +0530

    updated playlist view

--- app/views/playlist/list.blade.php
+++ app/views/playlist/list.blade.php
@@ -1,19 +1,16 @@
 <h1>Playlists</h1>
 
-<a href=/playlist/0>watch</a><br />
+
 
 <?php
 
-$tag_id = Tags::getId('todo');
-
-if ($tag_id)
-{
-    echo '<a href=/tag/' . $tag_id . '>todo</a><br />';
-}
-
 foreach($playlists as $playlist)
 {
-    echo "<a href=/playlist/" . $playlist->id . ">" . $playlist->name  . "</a> (" . $playlist->total . ") <a href=/playlist/empty/" . $playlist->id . ">x</a><br>";
+    echo "<div class=\"btn-group\">";
+    echo "<a href=/playlist/" . $playlist->id . " class=\"btn btn-success\">" . $playlist->name  . " <span class=\"badge\">" . $playlist->total . "</span></a>";
+    echo "<a href=/playlist/empty/" . $playlist->id . " class=\"btn btn-danger\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span></a>";
+    echo "</div>";
+    echo "<br> <br>";
 }
 
 ?>
@@ -23,3 +20,13 @@ foreach($playlists as $playlist)
 <a href=/playlist_make/2 class="btn btn-success">Best Medias</a>
 <a href=/playlist_make/1 class="btn btn-success">Best Tags</a>
 <a href=/settings/todays  class="btn btn-success">Build Todays</a>
+
+<?php
+
+$tag_id = Tags::getId('todo');
+if ($tag_id) {
+    echo '<a href=/tag/' . $tag_id . ' class="btn btn-success">Todo</a>';
+}
+
+?>
+
