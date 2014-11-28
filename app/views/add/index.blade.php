a349fd15108fc745311b7c6ae114da164099dbd9
WebHostingNeeds.com <info@webhostingneeds.com>
2014-11-28 14:45:17 +0530

    updated view

--- app/views/add/index.blade.php
+++ app/views/add/index.blade.php
@@ -2,30 +2,34 @@
 
 @section('content')
 
-<div class="page-header">
-    <h1>Add Records</h1>
-</div>
+<div class="container">
 
-<div class="row">
-    <div class="col-md-8">
-        <form action="/add/save" method="post"  class="form-horizontal">
-            <div class="form-group">
-                <label class="col-md-3" for="">File Name</label>
-                <div class="col-md-4">
-                    <input type="text" name="file_name" value="" autocomplete="off" class="form-control">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-md-7">
-                    <input type="submit" name="submit" value="Add" class="btn btn-success btn-block">
-                </div>
-            </div>
-
-        </form>
+    <div class="page-header">
+        <h1>Add Records</h1>
     </div>
-</div>
 
-@include('tools.links')
+    <div class="row">
+        <div class="col-md-8">
+            <form action="/add/save" method="post"  class="form-horizontal">
+                <div class="form-group">
+                    <label class="col-md-3" for="">File Name</label>
+                    <div class="col-md-4">
+                        <input type="text" name="file_name" value="" autocomplete="off" class="form-control">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-7">
+                        <input type="submit" name="submit" value="Add" class="btn btn-success btn-block">
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    @include('tools.links')
+
+</div>
 
 @stop
